d9895647a46611983cc6c6dc2f51f7efcdf7661c
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-14 13:01:59 -0500

    Add two methods for updating state whenever the input is changed and for calling the filterList method and passing the input value into it.

--- src/components/SearchEmployee.js
+++ src/components/SearchEmployee.js
@@ -2,11 +2,34 @@ import React, {Component} from "react";
 
 
 class SearchEmployee extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {input: ""};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.filterList(this.state.input);
+        this.setState({input: ""});
+    }
+
+    handleChange(evt) {
+        evt.preventDefault();
+        this.setState({
+            [evt.target.name] : evt.target.value
+        });
+    }
+    
     render() {
         return (
             <div className="SearchEmployee">
-                <form>
-                    <input placeholder="Find an employee..."/>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        name="input"
+                        value={this.state.input}
+                        onChange={this.handleChange}
+                        placeholder="Find an employee..."/>
                     <button>Search</button>
                 </form>
             </div>
