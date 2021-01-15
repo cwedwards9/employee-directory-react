693e26d4666961db4524b083ba7a7728943c8e8f
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-15 15:04:40 -0500

    Move SearchEmployee component to EmployeeList component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import Header from "./components/Header";
 import EmployeeList from "./components/EmployeeList";
-import SearchEmployee from "./components/SearchEmployee";
 import './App.css';
 
 class App extends Component {
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <SearchEmployee />
         <EmployeeList />
       </div>
     );
